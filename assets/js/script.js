a79754c084595e5e82bcfc3fe76ef1425a6c0b2f
zelstart <e.pomeroy95@gmail.com>
2023-08-04 18:13:03 -0400

    added more comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,13 @@ $(document).ready(function () {
         $('#search-bar').val("");
         saveSearch(searchBarText);
         updateSearchHistory();
-        cityCoord(searchHistoryList)
+        fetchCityCoord(searchHistoryList)
     })
 
-    function cityCoord(cityName) {
+    // add a keydown listener for search bar so that hitting the enter key has the same effect as hitting the submit button
+
+    // grab coordinates for user-entered city to use in fetchWeather function
+    function fetchCityCoord(cityName) {
         var cityQueryURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=7a0c14487898bae146a1b3a3863031d0";
         console.log(cityQueryURL) // checking to make sure it's grabbing the right name
         $.ajax({
@@ -34,15 +37,16 @@ $(document).ready(function () {
 
     }
 
-
+    // grab weather data
     function fetchWeather(forecastQueryURL) {
         $.ajax({
             url: forecastQueryURL,
             method: "GET",
             success: function (weatherResponse) {
-                var weatherData = []; // an empty container to store the weather data
+                var sixDayWeatherData = []; // an empty container to store the weather data for six days
                 for (var i = 0; i < 6; i++) { // I only need 6 days worth of data.
-                    //but because the timestamps are every three hours and not daily, it's giving me the same day multiple times instead of a new day in each obj
+                    // but because the timestamps are every three hours and not daily, it's giving
+                    // me the same day multiple times instead of a new day in each obj
                     var timestamp = weatherResponse.list[i].dt; // date timestamp -- will need to convert to mm/dd/yyyy format with dayjs
                     var date = formatDate(timestamp); // convert timestamp with function using dayjs
                     var icon = weatherResponse.list[i].weather[0].icon; // weather icon
@@ -50,8 +54,9 @@ $(document).ready(function () {
                     var desc = weatherResponse.list[i].weather[0].description; // weather desc
                     var temp = weatherResponse.list[i].main.temp; // temperature
                     var humidity = weatherResponse.list[i].main.humidity; // humidity
-                    
-                    var dailyWeatherData = { // make a new object containing info for each day made in the loop
+
+                    var dailyWeatherData = { // make a new object containing info for each day made in the loop. 
+                        // BUG: currently does not make a new object for each day, but for every 3 hours. 
                         timestamp: timestamp,
                         date: date,
                         iconURL: iconURL,
@@ -59,9 +64,9 @@ $(document).ready(function () {
                         temperature: temp,
                         humidity: humidity,
                     }
-                    weatherData.push(dailyWeatherData)
+                    sixDayWeatherData.push(dailyWeatherData)
                 }
-                console.log(weatherData)
+                console.log(sixDayWeatherData) // to check if we're getting the dates we want
             },
             error: function (error) {
                 console.log("There was an error while fetching weather data, please try again.")
@@ -76,15 +81,14 @@ $(document).ready(function () {
     }
 
 
-    // add a keydown listener for search bar so that hitting the enter key has the same effect as hitting the submit button
 
     // adds the latest search to searchHistoryList, storing only the 9 most recent searches
     function saveSearch(searchBarText) {
-        if (searchHistoryList.length === 9) {
-            searchHistoryList.pop();
-            searchHistoryList.unshift(searchBarText);
+        if (searchHistoryList.length === 9) { // check for an array already the max length we want
+            searchHistoryList.pop(); // remove last item in array
+            searchHistoryList.unshift(searchBarText); // add new item to the front of the array
         } else {
-            searchHistoryList.unshift(searchBarText);
+            searchHistoryList.unshift(searchBarText); // add new item to the front of the array
         }
         console.log(searchHistoryList) // should only have 9 most recent searches
     }
@@ -93,6 +97,7 @@ $(document).ready(function () {
     function updateSearchHistory() {
         $('#search-history').empty();
         for (var i = 0; i < searchHistoryList.length; i++) {
+            // would like to add an if statement that stops empty or invalid values from going through
             var searchItem = searchHistoryList[i];
             var historyButton = $('<button>').text(searchItem).addClass('btn cstm-btn-2');
             $('#search-history').append(historyButton);
